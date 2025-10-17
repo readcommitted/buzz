72f07a461205e8b78c592964177cc9a06d175e50
Matthew Scardino <75560559+readcommitted@users.noreply.github.com>
2025-10-16 23:18:15 -0600

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'buzzword-ball-v2';
+const CACHE = 'buzzword-ball-v3';
 const ASSETS = ['./','./index.html','./manifest.json','./phrases.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
